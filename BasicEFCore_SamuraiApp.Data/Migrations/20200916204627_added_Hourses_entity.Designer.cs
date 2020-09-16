5a6408e963ee6c374dd3ac03c64fbb86d058c973
Sagar Gupta <sagar.kr.gupta@outlook.com>
2020-09-17 02:33:07 +0545

    Init ial commit and implemented battle,hourse  entity

--- BasicEFCore_SamuraiApp.Data/Migrations/20200916204627_added_Hourses_entity.Designer.cs
+++ BasicEFCore_SamuraiApp.Data/Migrations/20200916204627_added_Hourses_entity.Designer.cs
@@ -4,14 +4,16 @@ using BasicEFCore_SamuraiApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BasicEFCore_SamuraiApp.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20200916204627_added_Hourses_entity")]
+    partial class added_Hourses_entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
